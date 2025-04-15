719843e97980b1bd9c153c838b766f33b412aa78
ThatCoolCoder <ThatCoolCoder@users.noreply.github.com>
2025-04-15 16:10:14 +0930

    fix bug in betternumberinput where it wouldn't update

--- app/components/overrides/BetterNumberInput.tsx
+++ app/components/overrides/BetterNumberInput.tsx
@@ -1,5 +1,5 @@
 import { NumberInput, type NumberInputProps } from "@mantine/core";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export interface BetterNumberInputProps extends NumberInputProps {
     onNumberChange: (n: number) => void
@@ -8,6 +8,11 @@ export interface BetterNumberInputProps extends NumberInputProps {
 // number input that won't return a string sometimes (stupid mantine)
 export function BetterNumberInput(props: BetterNumberInputProps) {
     const [stringValue, setStringValue] = useState(String(props.value));
+    const prevValue = useRef(props.value);
+
+    // If the value is updated outside the component, update our string value to match
+    if (prevValue.current != props.value) setStringValue(String(props.value));
+    prevValue.current = props.value;
 
     function onChange(v: string | number) {
         setStringValue(v.toString());
